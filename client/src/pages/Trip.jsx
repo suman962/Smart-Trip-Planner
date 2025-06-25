63638b34f7285889166cd40dc0f93b37ccfc6628
Suman <suman340s@proton.me>
2025-06-25 14:44:00 +0530

    added place and placephoto endpoints

---
+++ client/src/pages/Trip.jsx
@@ -0,0 +1,15 @@
+import Nav from '../components/nav';
+
+const Trip = () => {
+
+  return (
+    <div className="bg-[#214a68] h-screen">
+      <Nav currentPage='Search' />
+
+      ok
+    </div>
+  );
+}
+
+
+export default Trip;
--- server/routes/apis.js
+++ server/routes/apis.js
@@ -46,4 +46,55 @@ router.get("/autocomplete", async (req, res) => {
   }
 });
 
+router.get("/place", async (req, res) => {
+  try {
+    if (!req.query.id) {
+      return res.status(400).json({ error: "id query parameter is required"
+      });
+    }
+    const response = await axios({
+      method: 'GET',
+      url: `https://places.googleapis.com/v1/places/${req.query.id}`,
+      headers: {
+        'X-Goog-Api-Key': API_KEY,
+        'Content-Type': 'application/json',
+        'X-Goog-FieldMask': '*',
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching place details:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/placephoto", async (req, res) => {
+  try {
+    if (!req.query.photo) {
+      return res.status(400).json({ error: "photo query parameter is required"
+      });
+    }
+    const response = await axios({
+      method: 'GET',
+      url: `https://places.googleapis.com/v1/${req.query.photo}/media`,
+      headers: {
+        'X-Goog-Api-Key': API_KEY,
+        'Content-Type': 'application/json',
+      },
+      params: {
+        maxHeightPx: 1000,
+        key: API_KEY
+      },
+      responseType: 'arraybuffer'
+    });
+
+    res.set('Content-Type', response.headers['content-type']);
+    res.send(Buffer.from(response.data));
+
+  } catch (error) {
+    console.error("Error fetching place photo:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
