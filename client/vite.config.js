33f6cac930330640771dd8999c8365c663fb0346
Suman <suman340s@proton.me>
2025-06-27 22:32:35 +0530

    fixed big chunk issue when build

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('firebase')) return 'firebase';
+          if (id.includes('@heroui') || id.includes('framer-motion')) return 'ui';
+          if (id.includes('swiper')) return 'swiper';
+          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) return 'react';
+          if (id.includes('node_modules')) return 'vendor';
+        }
+      }
+    }
+  }
 })
